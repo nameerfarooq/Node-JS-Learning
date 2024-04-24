608754ba92307d2ee4b107a6e5209721439a7b19
Nameerfarooq <nameerfarooquit@gmail.com>
2024-04-24 18:06:36 +0500

    Dynamic Routing

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ app.use(function (req, res, next) {
   next();
 });
 app.use(function (req, res, next) {
-    console.log("Middleware Hello world 2");
-    next();
-  });
+  console.log("Middleware Hello world 2");
+  next();
+});
 app.get("/", function (req, res) {
   console.log("get 333 ");
   res.send("HEllo worl 1");
@@ -16,6 +16,9 @@ app.get("/", function (req, res) {
 app.get("/abc", function (req, res) {
   console.log("get 1 ");
   res.send("HEllo worl abc");
+});
+app.get("/profile/:username", (req, res) => {
+  res.send(`Hello from ${req.params.username}`);
 });
 app.listen("3000", () => {
   console.log("Connected");
