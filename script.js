029a2de3f4332031c1a424fdf8bfd223411045c5
Nameerfarooq <nameerfarooquit@gmail.com>
2024-04-24 16:52:55 +0500

    Basic Express Server With Middleware

---
+++ script.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(function (req, res, next) {
+  console.log("Middleware Hello world");
+  next();
+});
+app.use(function (req, res, next) {
+    console.log("Middleware Hello world 2");
+    next();
+  });
+app.get("/", function (req, res) {
+  console.log("get 333 ");
+  res.send("HEllo worl 1");
+});
+app.get("/abc", function (req, res) {
+  console.log("get 1 ");
+  res.send("HEllo worl abc");
+});
+app.listen("3000", () => {
+  console.log("Connected");
+});
