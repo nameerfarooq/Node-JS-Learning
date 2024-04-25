dc25f3ca5a06dd6af1744faabb89e3ee0dd5f9a7
Nameerfarooq <nameerfarooquit@gmail.com>
2024-04-25 14:47:35 +0500

    Static files added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("./public"));
 app.use(function (req, res, next) {
   console.log("Middleware Hello world");
   next();
